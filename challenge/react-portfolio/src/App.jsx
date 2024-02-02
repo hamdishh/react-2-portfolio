d383e0f8ec3a71029225df0e5ad65773be3d5ad9
Hamdi <hamdishh2003@gmail.com>
2024-02-02 18:46:22 +0000

    added new navbar

--- challenge/react-portfolio/src/App.jsx
+++ challenge/react-portfolio/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function NavTabs() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="about"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          About
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="blog"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Blog
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact/learn"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Learn
+        </NavLink>
+      </li>
+    </ul>
+  );
 }
 
-export default App
+export default NavTabs;
