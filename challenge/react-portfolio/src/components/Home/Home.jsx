48cb19289bfb529bba82158d421e5e8487b34b1b
Hamdi <hamdishh2003@gmail.com>
2024-02-06 01:02:03 +0000

    added styling to homepage

--- challenge/react-portfolio/src/components/Home/Home.jsx
+++ challenge/react-portfolio/src/components/Home/Home.jsx
@@ -1,2 +1,15 @@
 //remember path src/components/Home.js
 import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h1>Hamdi Sheikhabdullahi</h1>
+            <img src="/images/150x150.png" alt="Your Headshot Here" />
+            <p>What tf the brand statement is</p>
+            <p>Welcome to my portfolio showcasing my projects and skills as a developer</p>
+        </div>
+    );
+};
+
+export default Home;
