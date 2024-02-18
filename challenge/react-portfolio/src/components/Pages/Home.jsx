d21ec74ce66f95f663b1d4b3c26ba28193f61cc2
Hamdi <hamdishh2003@gmail.com>
2024-02-18 01:58:59 +0000

    adding more height to project cards' git push

--- challenge/react-portfolio/src/components/Pages/Home.jsx
+++ challenge/react-portfolio/src/components/Pages/Home.jsx
@@ -5,12 +5,12 @@ import Project from "../../components/Project";
 const Home = () => {
   return (
     <div className="home-container">
-      <h1>Hamdi Sheikhabdullahi</h1>
-      <img src={"../images/150x150.png"} alt="Your Headshot" />
-      <p>Brand statement</p>
-      <p>
+      {/* <h1>Project Showcase</h1> */}
+      {/* <img src={"../images/150x150.png"} alt="Your Headshot" />
+      <p>Brand statement</p> */}
+      {/* <p>
         Welcome to my portfolio showcasing my projects and skills as a developer
-      </p>
+      </p> */}
       <div className="project-container"> {/* Parent container for projects */}
 
       <Project
